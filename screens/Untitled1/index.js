7f08138dd808c7d013d1f8b23e4b55741abf1516
Daniel Palacios <daniel80380@gmail.com>
2023-01-23 10:35:37 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,15 +1,19 @@
+import { Pressable } from "react-native";
+import { View } from "react-native";
 import { TextInput } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const Untitled1 = () => {
+const Untitled1 = ({
+  navigation
+}) => {
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><TextInput style={styles.rnwToGyz}></TextInput></ScrollView>
+    }}><TextInput style={styles.rnwToGyz}></TextInput><Pressable onPress={() => navigation.navigation.navigate("undefined", {})}><View style={styles.BKwTrojI}></View></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
@@ -22,6 +26,13 @@ const styles = StyleSheet.create({
     borderColor: "#cccccc",
     width: 150,
     height: 30
+  },
+  BKwTrojI: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled1;
